e073a2f49d0202a65c90474bf0f3f34e7d0a68c8
zabed <zabedalam777@gmail.com>
2019-11-15 02:18:08 +0100

    delete done

--- index.js
+++ index.js
@@ -1,84 +1,96 @@
-const url = "https://strive-school-testing-apis.herokuapp.com/api/product/"
+const url = "https://strive-school-testing-apis.herokuapp.com/api/product/";
 
 window.onload = async () => {
-    let username = 'user20'
-    let password = 'Y2cJZ38UPMmnPdAW'
-    let token = btoa(username + ":" + password)
+  await loadProducts();
+};
 
-    let response = await fetch(url, {
+loadProducts = async () => {
+  let products = await getProducts();
+  console.log("PRODUCTS", products);
+  document.querySelector("#productColumn").innerHTML =
+    "<ul class='list-group'>" +
+    products
+      .map(
+        product => `<li class="list-group-item">
+      <div class="col-md-6>
+  <img src="${product.imageUrl}" style="width:100%">
+  <p>${product.name} - ${product.price}</p>
+  <input type="button" class="btn btn-danger" value="X" onclick="deleteRow('${product._id}')" />
+      </div>
+      `
+      )
+      .join("") +
+    " </ul>";
+};
 
-        headers: {
-
-            "Authorization": "Basic " + token
-        }
-    })
-    let jsonRes = await response.json();
-    console.log(jsonRes)
-
-    document.querySelector("#productColumn").innerHTML = jsonRes.map(product => `
-    <div class="col-md-6>
-<img src="${product.imageUrl}" style="width:100%">
-<p>${product.name} - ${product.price}</p>
-    </div>
-    `).join("")
-}
-
-// window.onload = async () => {
-//     let products = await createProduct();
-//     console.log("Products", products)
-// }
-// window.onload = async () => {
-//     let products = await getProduct()
-//     console.log("Products", products)
-//     let productsColumn = document.querySelector("#productColumn")
-//     if (products.length > 0) {
-//         productsColumn.innerHTML = products
-//             .map(product => `
-//         <div class="col-md-6">
-//         <img src="${product.imageUrl}" style="width:100%">
-//          <p>${product.name} - ${product.price}</p>
-//         </div>
-//         `).join("")
-//     } else {
-//         currentEventsDiv.innerText = "Please create new product"
-//     }
-// }
-
-getProduct = async (id) => {
-    let response = await fetch(url + id);
-    return await response.json();
-}
+getProducts = async () => {
+  let username = "user20";
+  let password = "Y2cJZ38UPMmnPdAW";
+  let token = btoa(username + ":" + password);
+  let response = await fetch(url, {
+    headers: {
+      Authorization: "Basic " + token
+    }
+  });
+  return await response.json();
+};
 handleSubmit = async () => {
-    event.preventDefault();
-    let myProduct = {
-        name: document.querySelector("#name").value,
-        description: document.querySelector("#description").value,
-        price: document.querySelector("#price").value,
-        brand: document.querySelector("#brand").value,
-        imageUrl: document.querySelector("#avatar").value
-    }
-    console.log("MyProduct", myProduct);
-    let response = await createProduct(myProduct)
-    // console.log(response)
-    if (response.ok) {
-        alert("The product saved Successfully")
-    } else {
-        alert("The product was not saved")
-    }
-}
-createProduct = async (product) => {
-    let username = 'user20'
-    let password = 'Y2cJZ38UPMmnPdAW'
-    let token = btoa(username + ":" + password)
-    let resp = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(product),
-        headers: {
-            "Authorization": "Basic " + token,
-            "Content-type": "application/json"
-        }
-    })
-    let jsonRes = await resp.json()
-    // console.log(jsonRes)
+  event.preventDefault();
+  let myProduct = {
+    name: document.querySelector("#name").value,
+    description: document.querySelector("#description").value,
+    price: document.querySelector("#price").value,
+    brand: document.querySelector("#brand").value,
+    imageUrl: document.querySelector("#avatar").value
+  };
 
-}
+  console.log("MyProduct", JSON.stringify(myProduct));
+  let response = await createProduct(myProduct);
+  console.log(response);
+  if (response) {
+    await loadProducts();
+  } else {
+    alert("The product was not saved");
+  }
+};
+
+createProduct = async product => {
+  let username = "user20";
+  let password = "Y2cJZ38UPMmnPdAW";
+  let token = btoa(username + ":" + password);
+  let resp = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(product),
+    headers: {
+      Authorization: "Basic " + token,
+      "Content-type": "application/json"
+    }
+  });
+
+  return resp;
+};
+
+deleteProduct = async id => {
+  let username = "user20";
+  let password = "Y2cJZ38UPMmnPdAW";
+  let token = btoa(username + ":" + password);
+  let reponse = await fetch(url + id, {
+    method: "DELETE",
+    headers: {
+      Authorization: "Basic " + token
+    }
+  });
+  return reponse;
+};
+
+deleteRow = async id => {
+  let button = event.currentTarget;
+  let success = await deleteProduct(id);
+
+  if (success) {
+    var li = button.closest(".list-group-item");
+    li.parentElement.removeChild(li);
+  } else {
+    alert("Something went worng!Please try later");
+  }
+};
